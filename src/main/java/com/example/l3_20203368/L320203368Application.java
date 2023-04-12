fb94946ecc97626f04b03344d818444fa03bcfd8
sebas1310 <a20203369@pucp.edu.pe>
2023-04-11 20:39:51 -0500

    L3

---
+++ src/main/java/com/example/l3_20203368/L320203368Application.java
@@ -0,0 +1,13 @@
+package com.example.l3_20203368;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L320203368Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L320203368Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/l3_20203368/L320203368ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l3_20203368;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L320203368ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
