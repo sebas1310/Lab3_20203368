aec54154ac3bf7b7d53fb7b90048fbbc3b0d4f6e
sebas1310 <a20203369@pucp.edu.pe>
2023-04-11 21:59:28 -0500

    :'(

--- src/main/java/com/example/l3_20203368/Repository/DocRepository.java
+++ src/main/java/com/example/l3_20203368/Repository/DocRepository.java
@@ -2,6 +2,7 @@ package com.example.l3_20203368.Repository;
 
 import com.example.l3_20203368.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DocRepository extends JpaRepository<Doctor,Integer> {
 
